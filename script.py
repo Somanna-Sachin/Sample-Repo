55efdb44f30250de0136fdc7eb588673e89f6f00
sachin <sachin.soman@gmail.com>
2019-11-24 10:02:49 -0600

    Chnges to the print

--- script.py
+++ script.py
@@ -1,10 +1,21 @@
+import sys
+
 import requests
 
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+print(greet("World"))
+print(greet("Somanna"))
 name = input("Your name? ")
 print("Hello,", name)
 r = requests.get("http://realpython.com")
 print(r.status_code)
-print(r.ok)
-
-
 print("somanna")
